4cf21d70d1aa2616d13ded00cfca6be1a3783df7
parentClass <dimalantadaniel@gmail.com>
2017-07-16 11:27:26 +0800

    api updated

--- src/public/api/v1/index.php
+++ src/public/api/v1/index.php
@@ -10,8 +10,44 @@ require 'vendor/autoload.php';
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$pdo = new PDO("mysql:host=localhost;"."dbname=id1855654_photopost","id1855654_admin","4Z32GHC5P8CG");
+$pdo = new PDO("mysql:host=localhost;"."dbname=id1855654_thenotes","id1855654_admin","09159177693cT");
 
 $app = new \Slim\App(["settings"=>$config]);
+
+$app->post('/validate_user', function (Request $request, Response $response) use ($pdo) {
+	$status = array();
+	if(!empty($request->getParsedBody()['username']) && !empty($request->getParsedBody()['passkey'])){
+		$username = $request->getParsedBody()['username'];
+		$password = md5($request->getParsedBody()['passkey']);
+
+		$outcome = $pdo->query("SELECT * FROM gcalc_users WHERE username='".$username."' AND passkey='".$password."'");
+		$outcome = $outcome->fetchAll(PDO::FETCH_ASSOC);
+
+		if(!empty($outcome)){
+			array_push($status, [
+				"status" => 200,
+				"validate_user_success" => true,
+				"is_valid" => true
+			]);
+		}else{
+			array_push($status, [
+				"status" => 200,
+				"validate_user_success" => true,
+				"is_valid" => false
+			]);
+		}
+
+	}else{
+		array_push($status, [
+			"status" => 200,
+			"validate_user" => false,
+			"error_occurred" => [
+				"1" => "username or passkey is empty"
+			]
+		]);
+	}
+
+    return json_encode($status);
+});
 
 $app->run();
